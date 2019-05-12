fd9cac49552ee02be49459b290bf4238cdd41ad6
Zahari Tudzharov <zhistler@gmail.com>
2019-05-12 20:25:04 +0300

    more readable sql and one corrected request in pc

--- week09/01/first_queries.sql
+++ week09/01/first_queries.sql
@@ -1,29 +1,26 @@
 SELECT address
-FROM   studio
-WHERE  name = 'MGM';
+  FROM studio
+ WHERE name = 'MGM';
 
 SELECT birthdate
-FROM   moviestar
-WHERE  name = 'Kim Basinger';
+  FROM moviestar
+ WHERE name = 'Kim Basinger';
 
 SELECT name
-FROM   movieexec
-WHERE  networth > 10000000;
+  FROM movieexec
+ WHERE networth > 10000000;
 
 SELECT name
-FROM   moviestar
-WHERE  gender = 'M'
-        OR address = 'Prefect Rd.';
+  FROM moviestar
+ WHERE gender = 'M'
+    OR address = 'Prefect Rd.';
 
 INSERT INTO moviestar
-VALUES      ('Zahari Baharov',
-             'Boulevard Vasil Levski',
-             'M',
-             '1900-01-01');
+VALUES      ('Zahari Baharov', 'Boulevard Vasil Levski', 'M', '1900-01-01');
 
 DELETE FROM studio
-WHERE  address LIKE '%5%';
+ WHERE address LIKE '%5%';
 
 UPDATE movie
-SET    studioname = 'Fox'
-WHERE  title LIKE '%Star%';  
+   SET studioname = 'Fox'
+ WHERE title LIKE '%Star%';  
--- week09/01/relations.sql
+++ week09/01/relations.sql
@@ -1,36 +1,36 @@
 SELECT moviestar.name
-FROM   moviestar
+  FROM moviestar
        INNER JOIN starsin
                ON moviestar.name = starsin.starname
-WHERE  moviestar.gender = 'M'
+ WHERE moviestar.gender = 'M'
        AND starsin.movietitle = 'Terms of Endearment';
 
 SELECT starsin.starname
-FROM   starsin
+  FROM starsin
        INNER JOIN movie
                ON movie.title = starsin.movietitle
-WHERE  starsin.movieyear = 1995
-       AND movie.studioname = 'MGM';
+ WHERE starsin.movieyear = 1995
+   AND movie.studioname = 'MGM';
 
 ALTER TABLE studio
   ADD COLUMN president VARCHAR(255);
 
 UPDATE studio
-SET    president = 'Bill Clinton'
-WHERE  name = 'MGM';
+   SET president = 'Bill Clinton'
+ WHERE name = 'MGM';
 
 SELECT president
-FROM   studio
-WHERE  name = 'MGM';
+  FROM studio
+ WHERE name = 'MGM';
 
 SELECT title
-FROM   movie
-WHERE  length > (SELECT length
-                 FROM   movie
-                 WHERE  title = 'Gone With the Wind');
+  FROM movie
+ WHERE length > (SELECT length
+                   FROM movie
+                  WHERE title = 'Gone With the Wind');
 
 SELECT networth
-FROM   movieexec
-WHERE  networth > (SELECT networth
-                   FROM   movieexec
-                   WHERE  name = 'Merv Griffin');
+  FROM movieexec
+ WHERE networth > (SELECT networth
+                     FROM movieexec
+                    WHERE name = 'Merv Griffin');
